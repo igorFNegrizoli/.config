3c7550793f0185bd2e0af1b2a73a46f499430848
Igor França Negrizoli <igor.negrizoli@protonmail.com>
2021-10-13 15:22:26 -0300

    Fixed theme icons

--- awesome/themes/powerarrow-black/theme.lua
+++ awesome/themes/powerarrow-black/theme.lua
@@ -15,7 +15,7 @@ local math, string, os = math, string, os
 local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
 
 local theme                                     = {}
-theme.dir                                       = os.getenv("HOME") .. "/.config/awesome/themes/powerarrow-blue"
+theme.dir                                       = os.getenv("HOME") .. "/.config/awesome/themes/powerarrow-black"
 theme.wallpaper                                 = theme.dir .. "/wallpaper.jpg"
 theme.font                                      = "Noto Sans Regular 11"
 theme.taglist_font                              = "Noto Sans Regular 14"
