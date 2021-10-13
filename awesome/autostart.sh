1b1a9d458d700b608ba00de914a46bff1c29c997
Igor França Negrizoli <igor.negrizoli@protonmail.com>
2021-10-13 16:38:58 -0300

    Fixed wallpapper setting

--- awesome/autostart.sh
+++ awesome/autostart.sh
@@ -13,16 +13,16 @@ run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 run nm-applet
 #run caffeine
 run pamac-tray
-run variety
+#run variety
 run xfce4-power-manager
 run blueberry-tray
 run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 run numlockx on
 run volumeicon
-#run nitrogen --restore
+run nitrogen --restore
 run conky -c $HOME/.config/awesome/system-overview
 #you can set wallpapers in themes as well
-feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
+#feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
 #run applications from startup
 #run firefox
 #run atom
